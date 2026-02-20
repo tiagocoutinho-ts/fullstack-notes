fcb2fe555d082dcff702184dc3c1caf240508dfe
tiagocoutinho-ts <tiagocoutinhots@gmail.com>
2026-02-20 12:34:44 -0300

    Configured connection to database and schema models

---
+++ backend/models/Note.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const NoteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+}, { timestamps: true })
+
+export default mongoose.model("Note", NoteSchema)
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,11 +1,19 @@
+import "dotenv/config"
 import express from "express"
+import mongoose from "mongoose"
+
 import cors from "cors"
 import { route } from "./router.js"
 const app = express()
-const PORT = 3002
+const PORT = 3002 
 
 app.use(express.json())
 app.use(cors())
 app.use(route)
 
-app.listen(PORT, ()=> console.log(`http://localhost:${PORT}`))
+//DATABASE CONNECTION
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  app.listen(PORT, () => console.log(`conectado ao MongoDB com sucesso! | app: http://localhost:${PORT}`))
+}).catch((err) => {
+  console.error("Erro ao conectar no MongoDB:", err.message)
+})
